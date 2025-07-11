13f14442c6e1640245c77fcf3e80f5762e5d65c2
Виталий <ipbvv@mail.ru>
2025-07-11 07:15:17 +0300

    Finish the "templating" lesson

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,6 +1,11 @@
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def index(request):
-    return HttpResponse('Article')
-# Create your views here.
+def index(request, ):
+    return render(
+        request,
+        'articles/index.html',
+        context={'Title': 'ARTICLEs'},
+    )
+#    return HttpResponse('Article')
--- hexlet_django_blog/settings.py
+++ hexlet_django_blog/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.debug',
             ],
         },
     },
@@ -122,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = ['127.0.0.1']
