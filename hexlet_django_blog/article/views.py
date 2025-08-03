ae91cc025360807b85e36be850cd1b6250c182d3
Виталий <ipbvv@mail.ru>
2025-08-03 10:35:27 +0300

    The lesson "Representations" will end

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,11 +1,15 @@
-#from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse
+#from django.shortcuts import render
+from django.views import View
 
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('Article')
 
-def index(request,):
-    return render(
-        request,
-        'articles/index.html',
-        context={'Title': 'ARTICLEs'},
-    )
+#def index(request,):
+#    return render(
+#        request,
+#        'articles/index.html',
+#        context={'Title': 'ARTICLEs'},
+#    )
 #    return HttpResponse('Article')
--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -12,16 +12,6 @@ class HomePageView(TemplateView):
         return context
 
 
-
-
-#def index(request):
-#    return render(
-#        request,
-#        "index.html",
-#        context={ "who": "World",},
-#    )
-
-
 def about(request):
     tags = ["обучение", "программирование", "python", "oop"]
     return render(request, "about.html", context={'tags': tags})
