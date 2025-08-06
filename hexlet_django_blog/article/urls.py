a1cf6130d9f2e8da766db8642e22ab43a209955e
Виталий <ipbvv@mail.ru>
2025-08-06 05:21:05 +0300

    The lesson "CRUD" will end

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from hexlet_django_blog.article import views
+#from hexlet_django_blog.article import views
+from hexlet_django_blog.article.views import IndexView, index
 
 
 urlpatterns = [
-    path('', views.IndexView.as_view()),
-    path('<str:tags>/<int:article_id>', views.index, name='article')
+    path('', IndexView.as_view()),
+    path('<str:tags>/<int:article_id>', index, name='article')
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,14 +1,19 @@
 #from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
-from django.urls import reverse
-from django.shortcuts import redirect
+#from django.shortcuts import redirect
 #from django.views.decorators.http import require_http_methods
+from hexlet_django_blog.article.models import Article
+
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        url = reverse("article", kwargs={'tags': 'python', 'article_id': 42})
-        return redirect(url)
+        articles = Article.objects.all()[:15]
+        return render(request, 'articles/index.html', context={'articles': articles})
+
+#    def get(self, request, *args, **kwargs):
+#       url = reverse("article", kwargs={'tags': 'python', 'article_id': 42})
+#        return redirect(url)
 
 def index(request, tags, article_id):
     return render(
